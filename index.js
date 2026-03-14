0694c37b6630144fb9b02197cb8a517f96b6c443
khianvictorycalderon <ckhianvictory@gmail.com>
2026-03-14 10:31:28 +0800

    Added test route

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const PORT = 3000;
+
+// Start the app
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (_req, res) => {
+    res.send("API is working!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
