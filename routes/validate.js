88dc48645d4716b44e25c5510445c9a205ab2d4f
khianvictorycalderon <ckhianvictory@gmail.com>
2026-03-14 10:50:19 +0800

    Added validator route

---
+++ routes/validate.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const VALID_TOKENS = require("../valid_tokens");
+
+router.post("/", (req, res) => {
+    
+});
+
+module.exports = router;
